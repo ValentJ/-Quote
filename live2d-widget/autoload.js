ea4d337d531de9b3dd4fecfae5c2affe58f4d88d
ValentJ <58519221+ValentJ@users.noreply.github.com>
2019-12-06 12:18:22 +0800

    Update autoload.js

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,5 +1,5 @@
 //注意：live2d_path参数应使用绝对路径
-const live2d_path = "https://github.com/ValentJ/-Quote/live2d-widget/";
+const live2d_path = "https://github.com/ValentJ/-Quote/tree/master/live2d-widget";
 //const live2d_path = "/live2d-widget/";
 
 //封装异步加载资源的方法
